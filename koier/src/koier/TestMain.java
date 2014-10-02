d39a37f9fe51982d985b8278b5162acc20f8fa1d
eirikasp <eirihans@gmail.com>
2014-10-02 13:52:08 +0200

    Mainfiltest

---
+++ koier/src/koier/TestMain.java
@@ -0,0 +1,11 @@
+package koier;
+
+public class TestMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		javafx.application.Application.launch(Login.class);
+		
+	}
+
+}
