2b8d69d36e8cbf2331e49f8e863bd8d45ce0794a
mariaasp <marianasp@live.no>
2014-10-31 13:34:20 +0100

    ny meny

--- koier/src/koier/Meny.java
+++ koier/src/koier/Meny.java
@@ -39,46 +39,52 @@ public class Meny extends Application {
 		
 		
 		Button reservasjon = new Button();
-		reservasjon.setLayoutX(65.0);
-		reservasjon.setLayoutY(132.0);
-		reservasjon.setPrefHeight(69.0);
-		reservasjon.setPrefWidth(145.0);
-		reservasjon.setText("Reservasjon");
+		reservasjon.setLayoutX(75.0);
+		reservasjon.setLayoutY(137.0);
+		reservasjon.setPrefHeight(59.0);
+		reservasjon.setPrefWidth(169.0);
+		reservasjon.setText("Reserver koie");
 		reservasjon.setFont(new Font("System", 18));
 		
 		Button rapport = new Button();
-		rapport.setLayoutX(65.0);
-		rapport.setLayoutY(270.0);
-		rapport.setPrefHeight(69.0);
-		rapport.setPrefWidth(145.0);
-		rapport.setText("Rapport");
+		rapport.setLayoutX(75.0);
+		rapport.setLayoutY(230.0);
+		rapport.setPrefHeight(59.0);
+		rapport.setPrefWidth(169.0);
+		rapport.setText("Skriv rapport");
 		rapport.setFont(new Font("System", 18));
 		
 		Button loggUt = new Button();
-		loggUt.setLayoutX(347.0);
-		loggUt.setLayoutY(299.0);
+		loggUt.setLayoutX(220.0);
+		loggUt.setLayoutY(330.0);
 		loggUt.setPrefHeight(40.0);
-		loggUt.setPrefWidth(145.0);
+		loggUt.setPrefWidth(137.0);
 		loggUt.setText("Logg ut");
 		loggUt.setFont(new Font("System", 18));
 		
+		Label adminLabel = new Label(" For Administrator: ");
+		adminLabel.setPrefHeight(40);
+		adminLabel.setPrefWidth(137);
+		adminLabel.setLayoutX(343);
+		adminLabel.setLayoutY(100);
+		
 		Button admRap = new Button();
-		admRap.setLayoutX(347.0);
-		admRap.setLayoutY(132);
-		admRap.setPrefHeight(69.0);
-		admRap.setPrefWidth(145.0);
-		admRap.setText("Adminrapport");
+		admRap.setLayoutX(339.0);
+		admRap.setLayoutY(137.0);
+		admRap.setPrefHeight(59.0);
+		admRap.setPrefWidth(169.0);
+		admRap.setText("Rapporter");
 		admRap.setFont(new Font("System", 18));
 		
 		Button resRap = new Button();
-		resRap.setLayoutX(0.0);
-		resRap.setLayoutY(0);
-		resRap.setPrefHeight(69.0);
-		resRap.setPrefWidth(145.0);
-		resRap.setText("Adminrapport");
+		resRap.setLayoutX(339.0);
+		resRap.setLayoutY(230.0);
+		resRap.setPrefHeight(59.0);
+		resRap.setPrefWidth(169.0);
+		resRap.setText("Reservasjoner");
 		resRap.setFont(new Font("System", 18));
 		
-		pane.getChildren().addAll(velkommen, reservasjon, rapport, loggUt, admRap,resRap);
+		pane.getChildren().addAll(velkommen, reservasjon, rapport, loggUt, admRap,resRap, adminLabel);
 		primaryStage.show();
 		
 		reservasjon.setOnAction(new EventHandler<ActionEvent>(){
