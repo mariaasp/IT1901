4e12ec3d4092ed7de65f5d46c397ed5de9e7978b
eirikasp <eirihans@gmail.com>
2014-11-02 13:45:21 +0100

    Oppdatert skaderap for adming

--- koier/src/koier/AdmRap.java
+++ koier/src/koier/AdmRap.java
@@ -6,8 +6,8 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.LocalDate;
+
 import javafx.application.Application;
-import javafx.application.Platform;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
@@ -18,11 +18,13 @@ import javafx.geometry.Insets;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
@@ -34,10 +36,14 @@ import javafx.util.Callback;
 
 public class AdmRap extends Application {
 	
-	TextField textField_name;
-	TextField textField_lastname;
-	TextField textField_email;
+	Bruker temp;
+	TextField textField_fornavn;
+	TextField textField_etternavn;
+	TextField textField_mobilnr;
 	int skadeNrSend;
+	TextArea test;
+	TextField  textField_ved;
+	
 	
 	public static class Record {
 
@@ -48,11 +54,10 @@ public class AdmRap extends Application {
 		private final SimpleIntegerProperty brukerId;
 		private final SimpleStringProperty repDato;
 		private final SimpleIntegerProperty adminId;
+		private final SimpleStringProperty ved;
 
-		private Record(final int skadeId, final int koieNr, final String skadeDato, final String skade, final int brukerId, final String repDato, final int adminId) {
-
-			System.out.println("Platform.isFxApplicationThread(): "
-					+ Platform.isFxApplicationThread());
+		private Record(final int skadeId, final int koieNr, final String skadeDato, final String skade, final int brukerId, final String repDato, final int adminId,final boolean booly) {
+			
 			this.skadeId = new SimpleIntegerProperty(skadeId);
 			this.koieNr = new SimpleIntegerProperty(koieNr);
 			this.skadeDato = new SimpleStringProperty(skadeDato);
@@ -60,6 +65,25 @@ public class AdmRap extends Application {
 			this.brukerId = new SimpleIntegerProperty(brukerId);
 			this.repDato = new SimpleStringProperty(repDato);
 			this.adminId = new SimpleIntegerProperty(adminId);
+			if(booly){
+				this.ved = new SimpleStringProperty("Ja");
+			}
+			else{
+				this.ved = new SimpleStringProperty("Nei");
+			}
+			
+		}
+		public String getVed(){
+			return this.ved.get();
+		}
+		public void setVed(final boolean bool){
+			if(bool){
+				this.ved.set("Ja");
+			}
+			else{
+				this.ved.set("Nei");
+			}
+			
 		}
 		public String getSkadeDato(){
 			return this.skadeDato.get();
@@ -132,7 +156,7 @@ public class AdmRap extends Application {
 			ResultSet results = statement.executeQuery();
 			for(int i = 0; i < 50; i++){
 				results.next();
-				rapportList.add(new Record(results.getInt(1),results.getInt(2),results.getString(3),results.getString(4),results.getInt(5),results.getString(6),results.getInt(7)));
+				rapportList.add(new Record(results.getInt(1),results.getInt(2),results.getString(3),results.getString(4),results.getInt(5),results.getString(6),results.getInt(7),true));
 			}
 			con.close();
 		} catch (Exception e) {	
@@ -148,12 +172,13 @@ public class AdmRap extends Application {
 	@SuppressWarnings({ "rawtypes", "unchecked" })
 	public void start(final Stage primaryStage, Bruker bruker) throws SQLException {
 		final Scene scene = new Scene(new Group());
+		temp = bruker;
 		primaryStage.setTitle("Rapport");
 		primaryStage.setWidth(800);
 		primaryStage.setHeight(800);
 
 		hentRapport();
-
+			
 		tableView.setEditable(false);
 
 		final Callback<TableColumn, TableCell> integerCellFactory =
@@ -204,18 +229,22 @@ public class AdmRap extends Application {
 		colAdminId.setCellValueFactory(new PropertyValueFactory<Record, String>("adminId"));
 		colAdminId.setCellFactory(integerCellFactory);
 		
+		TableColumn colVed = new TableColumn("Vedstatus");
+		colVed.setCellValueFactory(new PropertyValueFactory<Record, String>("ved"));
+		colVed.setCellFactory(stringCellFactory);
+		
 		tableView.setItems(rapportList);
-		tableView.getColumns().addAll(colId, colKoieNr, colSkadeDato, colSkade, colBrukerId, colRepDato, colAdminId);
+		tableView.getColumns().addAll(colId, colKoieNr, colSkadeDato, colSkade, colBrukerId, colRepDato, colAdminId, colVed);
 
 		final VBox vbox = new VBox();
 		
 		Label label_dato = new Label("Reperasjonsdato");
 		DatePicker dato = new DatePicker();
-		dato.setPromptText("Reservasjonsdato");
+		dato.setPromptText("Reperasjonsdato");
 		HBox hBox_dato = new HBox();
 		hBox_dato.setSpacing(10);
 		hBox_dato.getChildren().addAll(label_dato, dato);
-
+		
 		Button regButt = new Button("Registrer");
 		regButt.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
@@ -229,7 +258,7 @@ public class AdmRap extends Application {
 				}
 			}
 		});
-		Button cancel = new Button("Avbryt");
+		Button cancel = new Button("Tilbake");
 		
 		cancel.setOnAction(new EventHandler<ActionEvent>(){
 			public void handle(ActionEvent event){
@@ -241,16 +270,68 @@ public class AdmRap extends Application {
 				primaryStage.close();
 			}
 		});
+
+		Label turleder = new Label("Turleder");
+		Label fornavn = new Label("Fornavn");
+		Label etternavn = new Label("Etternavn");
+		Label mobil = new Label("Mobilnr");
+		textField_fornavn = new TextField();
+		textField_etternavn = new TextField();
+		textField_mobilnr = new TextField();
+		textField_fornavn.editableProperty().set(false);
+		textField_etternavn.editableProperty().set(false);
+		textField_mobilnr.editableProperty().set(false);
+		HBox hBox_fornavn = new HBox();
+		hBox_fornavn.setSpacing(10);
+		hBox_fornavn.getChildren().addAll(fornavn, textField_fornavn);
+		HBox hBox_etternavn = new HBox();
+		hBox_etternavn.setSpacing(10);
+		hBox_etternavn.getChildren().addAll(etternavn, textField_etternavn);
+		HBox hBox_mobil = new HBox();
+		hBox_mobil.setSpacing(10);
+		hBox_mobil.getChildren().addAll(mobil, textField_mobilnr);
+		textField_ved = new TextField();
+		textField_ved.editableProperty().set(false);
+		Label ved = new Label("Nok ved");
+		HBox hBox_ved = new HBox();
+		hBox_ved.setSpacing(10);
+		hBox_ved.getChildren().addAll(ved,textField_ved);
+		
+		VBox vBox_person = new VBox();
+		vBox_person.setSpacing(10);
+		vBox_person.getChildren().addAll(turleder,hBox_fornavn,hBox_etternavn,hBox_mobil,hBox_ved);
+		test = new TextArea();
+		test.editableProperty().set(false);
+		Label label_beskrivelse = new Label("Skadebeskrivelse");
+		HBox hBox_besk = new HBox();
+		hBox_besk.setSpacing(10);
+		hBox_besk.getChildren().addAll(label_beskrivelse, test);
+		HBox hBox_alt = new HBox();
+		hBox_alt.setSpacing(10);
+		hBox_alt.getChildren().addAll(vBox_person,hBox_besk);
+		
+		HBox hBox_registrering = new HBox();
+		hBox_registrering.setSpacing(10);
+		Label merVed = new  Label("Mer ved lagt til");
+		CheckBox sjekkVed = new CheckBox();
+		Button regVed = new Button("Registrer ved");
+		hBox_registrering.getChildren().addAll(hBox_dato, regButt, merVed,sjekkVed,regVed);
+		
 		
 		vbox.setSpacing(5);
 		vbox.setPadding(new Insets(10, 0, 0, 10));
-		vbox.getChildren().addAll(hBox_dato,regButt, tableView, cancel);
+		vbox.getChildren().addAll(hBox_alt, tableView, hBox_registrering, cancel);
 
 		((Group) scene.getRoot()).getChildren().addAll(vbox);
 
 		primaryStage.setScene(scene);
 		primaryStage.show();
-	}
+		
+		regVed.setOnAction(new EventHandler<ActionEvent>(){
+			public void handle(ActionEvent event){
+				System.out.println(sjekkVed.isSelected());
+			}
+		});}
 
 	public static void main(final String[] args) {
 		launch(args);
@@ -303,6 +384,17 @@ public class AdmRap extends Application {
 				System.out.println("BrukerID = " + item.getBrukerId());
 				System.out.println("Reperasjonsdato = " + item.getRepDato());
 				System.out.println("AdminID = " + item.getAdminId());
+				System.out.println("Vedstatus = " + item.getVed());
+				test.setText(item.getSkade());
+				textField_fornavn.setText(temp.getFornavn());
+				textField_etternavn.setText(temp.getEtternavn());
+				StringBuilder sb = new StringBuilder();
+				sb.append("");
+				sb.append(temp.getMobilnr());
+				String mob = sb.toString();
+				textField_mobilnr.setText(mob);
+				textField_ved.setText(item.getVed());
+				
 			} catch (IndexOutOfBoundsException exception) {
 			}
 
